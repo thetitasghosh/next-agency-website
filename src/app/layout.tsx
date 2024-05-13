25197ef0c02742f228616176cbcb5bdfbe58a8b8
Titas Ghosh <rg814602@gmail.com>
2024-05-13 10:26:40 +0530

    Footer cmpo add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/section/Header";
-// import Footer from '@/components/section/Footer'
+import Footer from "@/components/section/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-screen h-40 flex items-center justify-center">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
