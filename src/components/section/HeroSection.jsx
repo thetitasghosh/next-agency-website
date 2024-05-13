108aca58a5804ca05e4520324ea1a775799f3e23
Titas Ghosh <rg814602@gmail.com>
2024-05-13 17:23:08 +0530

    add new button hero section style change

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-
+import Abutton from '@/components/ui/Abutton'
 const HeroSection = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center gap-5 justify-start py-28">
+    <div className="w-screen h-screen flex flex-col items-center gap-5 justify-start  py-28">
+      <Abutton/>
       <div className="HeroHeader w-full h-36  flex items-center justify-center flex-col text-4xl ">
         <h1 className="font-semibold">Digital Bandhan</h1>
         <h1>A Creative Agency</h1>
         <h1>For your need</h1>
       </div>
       <div className="HeroDesc w-full text-center h-20 text-base flex items-start justify-center   px-5">
-        <p>
+        <p className="leading-5">
           we are digital bandhan,a creative digital agency for your creative
           proccess
         </p>
