e104d4301fe7f87059497d80ba7b97a89f6ca299
Titas Ghosh <rg814602@gmail.com>
2024-05-13 09:29:00 +0530

    about section style change

--- src/components/section/AboutSection.jsx
+++ src/components/section/AboutSection.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 const AboutSection = () => {
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-start red gap-10">
-      <div className="w-full text-lg red ">
-        <h1 className="px-2">About us ...</h1>
+    <div className="w-screen min-h-96 flex flex-col items-center justify-start red gap-10">
+      <div className="w-full text-2xl  ">
+        <h1 className="px-2 font-medium py-5">About us ...</h1>
       </div>
-      <div className="w-full h-96 flex items-center justify-center red p-5">
-        <p className='text-sm font-medium text-left '>
-          Digital Bandhan is a Digital Marketing company in Bankura, West
-          Bengal. In order to develop any business in the present day, its
-          promotion is a must. It is possible to reach more people at a very low
-          cost, especially from digital marketing. Digital marketing becomes
-          extremely easy through the various services of DIGITAL BANDHAN. Which
-          also helps to take the business forward in this competitive market.
-          DIGITAL-BANDHANâ€™s various services such as Graphics Design,
-          Advertisement Video, Advertisement Voice, Facebook Marketing, various
-          digital mediums such as social media marketing, web marketing, and
-          offline marketing are also essential for business development. So,
-          Make Your Organization Famous with us.
+      <div className="w-full h-96 flex items-center justify-center  p-5">
+        <p className="text-lg font-medium text-left ">
+          Digital Bandhan, a Bankura-based digital marketing company, offers a
+          variety of services to help businesses grow in today competitive
+          market. From graphic design and social media marketing to video and
+          voice advertising, they provide essential tools for business
+          development at a low cost. Let Digital Bandhan help your organization
+          achieve fame.
         </p>
       </div>
     </div>
