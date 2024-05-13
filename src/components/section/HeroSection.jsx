06639eff52df63c4e61c48814fa3621d7502648a
Titas Ghosh <rg814602@gmail.com>
2024-05-13 18:06:29 +0530

    header desq change

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -3,7 +3,7 @@ import Abutton from '@/components/ui/Abutton'
 const HeroSection = () => {
   return (
     <div className="w-screen h-screen flex flex-col items-center gap-5 justify-start  py-28">
-      <Abutton/>
+      <Abutton />
       <div className="HeroHeader w-full h-36  flex items-center justify-center flex-col text-4xl ">
         <h1 className="font-semibold">Digital Bandhan</h1>
         <h1>A Creative Agency</h1>
@@ -11,8 +11,8 @@ const HeroSection = () => {
       </div>
       <div className="HeroDesc w-full text-center h-20 text-base flex items-start justify-center   px-5">
         <p className="leading-5">
-          we are digital bandhan,a creative digital agency for your creative
-          proccess
+          We equip ambitious brands and organizations to lead from the front
+          through strategic positioning and thoughtful storytelling.
         </p>
       </div>
     </div>
--- src/components/section/WorkSection.jsx
+++ src/components/section/WorkSection.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const WorkSection = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center'>
-      
+    <div className="w-screen min-h-screen flex flex-col items-center justify-start p-2">
+      <div className="w-full px-2">
+        <h1 className="text-2xl font-medium">Works</h1>
+      </div>
+      <div className="w-full h-screen red">
+        
+      </div>
     </div>
   );
-}
+};
 
 export default WorkSection;
