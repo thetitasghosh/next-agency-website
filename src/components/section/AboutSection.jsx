deb0449d352be8d695b878da49610ca94b598c0d
Titas Ghosh <rg814602@gmail.com>
2024-05-13 09:54:14 +0530

    style change

--- src/components/section/AboutSection.jsx
+++ src/components/section/AboutSection.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const AboutSection = () => {
   return (
-    <div className="w-screen min-h-96 flex flex-col items-center justify-start red gap-10">
+    <div className="w-screen min-h-96 flex flex-col items-center justify-start red gap-10 p-2">
       <div className="w-full text-2xl  ">
-        <h1 className="px-2 font-medium py-5">About us ...</h1>
+        <h1 className=" px-2 font-medium ">About us ...</h1>
       </div>
-      <div className="w-full h-96 flex items-center justify-center  p-5">
+      <div className="w-full h-80 flex items-center justify-center  p-5">
         <p className="text-lg font-medium text-left ">
           Digital Bandhan, a Bankura-based digital marketing company, offers a
           variety of services to help businesses grow in today competitive
