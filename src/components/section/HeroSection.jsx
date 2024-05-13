0992e6ad7f5fa87f8c02580310e7663c0dd8dea8
Titas Ghosh <rg814602@gmail.com>
2024-05-13 09:17:53 +0530

    hero section style change

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-start py-10">
-      <div className="HeroHeader w-full h-36  flex items-center justify-center flex-col text-3xl l">
-        <h1>Digital Bandhan</h1>
+    <div className="w-screen h-screen flex flex-col items-center gap-5 justify-start py-10">
+      <div className="HeroHeader w-full h-36  flex items-center justify-center flex-col text-4xl ">
+        <h1 className="font-semibold">Digital Bandhan</h1>
         <h1>A Creative Agency</h1>
         <h1>For your need</h1>
       </div>
       <div className="HeroDesc w-full text-center h-20 text-xs flex items-start justify-center   px-5">
-        <p>we are digital bandhan,a creative digital agency for your creative proccess</p>
+        <p>
+          we are digital bandhan,a creative digital agency for your creative
+          proccess
+        </p>
       </div>
     </div>
   );
