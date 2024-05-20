c16b3716f4ee5b461b7d05bf32a0bb04546a0d07
Titas Ghosh <rg814602@gmail.com>
2024-05-20 09:19:55 +0530

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ContactSection from "@/components/section/ContactSection";
 
 export default function Home() {
   return (
-    <main className="flex  min-h-screen  flex-col items-center justify-between p-5  mix-blend-normal bg-black">
+    <main className="flex w-screen  min-h-screen  flex-col items-center justify-between p-5   bg-black">
       <div className="flex flex-col w-[418px] h-full items-center justify-center  bg-white ">
         <HeroSection />
         <AboutSection />
