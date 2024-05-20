8f4dd01c64ff18cad3a825480a0be1572be5c305
Titas Ghosh <rg814602@gmail.com>
2024-05-20 17:51:52 +0530

    c

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -13,12 +13,12 @@ const HeroSection = () => {
     <>
       <div className="w-full h-screen flex flex-col items-center gap-1 justify-start  py-16">
         <Abutton />
-        <div className="hero-logo  w-full h-40  flex items-center justify-evenly px-2">
+        <div className="hero-logo  w-full h-40  flex items-center justify-evenly px-3 gap-1">
           <div className="size-36  flex items-center justify-center rounded-xl border border-black">
             logo
           </div>
-          <div className="flex flex-col items-center justify-evenly">
-            <h1 className="text-2xl font-bold">NGT SOFTWARE</h1>
+          <div className="flex flex-col items-start justify-evenly">
+            <h1 className="text-2xl font-bold ">NGT SOFTWARE</h1>
             <h1 className="text-xl font-medium">Digital Marketing Agency</h1>
           </div>
         </div>
