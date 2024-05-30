1761e426724cfa2248a0bc5280ce14e50a267e70
Titas Ghosh <rg814602@gmail.com>
2024-05-30 21:13:04 +0530

    app booton changes

--- src/components/ui/MBotton.jsx
+++ src/components/ui/MBotton.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const MBotton = () => {
   return (
     <div className="fixed w-full flex justify-center bottom-2 ">
-      <div className="w-[390px] h-10 red rounded-md">
-        
+      <div className="w-[370px] h-10 red rounded-md">
+
       </div>
     </div>
   );
