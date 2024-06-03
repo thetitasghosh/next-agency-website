06b174ae1a511f1e45d0b4f7429385fbc0c3af41
Titas Ghosh <rg814602@gmail.com>
2024-06-03 07:39:09 +0530

    add static img on workcard

--- src/data/workData.js
+++ src/data/workData.js
@@ -1,15 +1,17 @@
+import diamond from '../../public/img/work/diamond.jpg'
+import brand from '../../public/img/work//brand.png'
 
 export const workdata = [
   {
     id: 1,
-    img: "",
+    img: diamond,
     title: "Heading 1",
     desq: "description",
     date: "20.05.24",
   },
   {
     id: 2,
-    img: "",
+    img: brand,
     title: "Heading 2",
     desq: "description",
     date: "20.05.24",
