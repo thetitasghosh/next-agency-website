6c99ea0e8fea3fc07edffe61066b161dc04206de
Titas Ghosh <rg814602@gmail.com>
2024-06-30 19:01:51 +0530

    change appbutton color to green

--- src/components/ui/AppBotton.jsx
+++ src/components/ui/AppBotton.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const MBotton = () => {
   return (
     <div className="fixed bottom-2 flex w-full justify-center">
-      <div className="flex h-20 w-[370px] items-center justify-evenly rounded-md border border-black bg-neutral-300 py-8 bg-background/80 backdrop-blur-sm">
+      <div className="flex h-20 w-[370px] items-center justify-evenly rounded-md border border-black bg-green-600 py-8 ">
         {navigation.map((data, idx) => {
           return <NavButton key={idx} data={data} />;
         })}
@@ -26,8 +26,8 @@ const NavButton = ({ data }) => {
           setIsClick(!isClick);
         }}
         style={{
-          backgroundColor: isClick ? "#262626" : " ",
-          color: isClick ? "white" : "",
+          backgroundColor: isClick ? "#1bc95b" : " ",
+          color: isClick ? "white" : "white",
         }}
         className="flex size-16 cursor-pointer flex-col items-center justify-center rounded-md"
       >
